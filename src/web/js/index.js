a6f9807b543bd53f9642456055c95cad31d43029
Prashant-Naicker <prashantnaicker@gmail.com>
2017-04-28 16:41:04 +1200

    Started index.js and included jQuery into index.html

---
+++ src/web/js/index.js
@@ -0,0 +1,7 @@
+// Index.js.
+// Will make the button. when clicked send an HTTP POST request to my webserver
+// that contains what was written in the textbox.
+
+$(document).ready(function () {
+    var Button = $("#button-send");
+});
